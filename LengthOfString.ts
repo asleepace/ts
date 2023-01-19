3389eccf3b88b9b86da270a45f1055c01e14cade
Colin <colin_teahan@yahoo.com>
2023-01-18 22:17:12 -0800

    updated length of string

--- LengthOfString.ts
+++ LengthOfString.ts
@@ -4,13 +4,18 @@ type LengthOfString<
   IsEmptyString extends '' = '',
   StringLength extends Characters['length'] = Characters['length'],
 > =
+  // check if the string is empty (base case)
   String extends IsEmptyString ?
+    // if string is empty return current string length
     StringLength :
+    // else get the first character and rest as substring
     String extends `${infer First}${infer Rest}` ?
-      LengthOfString<Rest, [First, ...Characters]> :
-      StringLength
+        // recursively get length by appending first to character
+        LengthOfString<Rest, [First, ...Characters]> :
+        // return length of string if no more
+        StringLength
 
 // exmaple usage
-type Length1 = ""
-type Length2 = "abc"
-type Length3 = "abc123"
+type Length1 = LengthOfString<"">
+type Length2 = LengthOfString<"abc">
+type Length3 = LengthOfString<"abc123">
