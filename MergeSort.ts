fe93e473fd4e5d291604bcc73dc5bdc866e7caac
Colin <colin_teahan@yahoo.com>
2023-01-21 23:37:49 -0800

    Code working

--- MergeSort.ts
+++ MergeSort.ts
@@ -1,106 +1,90 @@
 
-// returns length of tuple
+// returns length of tuple T
 type Length<T extends any[]> = T['length']
 
-// make a tuple of a given size
+// returns a tuple of size S
 type Tuple<S extends number, T extends any[]=[]> = 
     Length<T> extends S ? T : Tuple<S, [...T, any]>
 
-// compare two numbers and return in ascending order
-type Compare<A extends number, B extends number> =
-    A extends B ? [A, B] :
-    Tuple<A> extends [...Tuple<B>, ...infer U] ? 
-        [B, A] : [A, B]
+// returns absolute value if a number is negative otherwise never
+type IsNegative<A extends number, S extends string = `${A}`> =
+    S extends `-${infer V extends number}` ? true : never
 
-
-type IsGreater<
-    A extends number, 
-    B extends number, 
+// returns true if A is greater than B otherwise never
+type IsGreater<A extends number, B extends number,
     BoxA extends any[] = Tuple<A>, BoxB extends any[] = Tuple<B>> =
         BoxA extends [...(BoxB), ...infer U] ? 
             true : never
 
-type test1 = Tuple<5>
-//   ^?
+// returns a sorted tuple of [A and B] based on IsGreater
+type SortPair<A extends number, B extends number> =
+    [IsGreater<A, B>] extends [true] ?
+        [B, A] : [A, B]            
 
-type test2 = Length<test1>
-//   ^?
-
-type test3 = Compare<1, 2>
-//   ^?
-
-type test4 = Compare<4, 4>
-//   ^?
-
-type test5 = Compare<5, 7>
-//   ^?
-
-type test6 = IsGreater<5, 8>
-//   ^?
-
-type test7 = IsGreater<6, 5>
-//   ^?
-
-type test8 = IsGreater<5, 6>
-//   ^?
-
-// Stacking Boxes
-[
-    [],
-    [[],[],[]],
-    [[], []],
-]
+// returns a tuple of A and B in ascending order
+type Compare<A extends number, B extends number> =
+    [IsGreater<A, B>] extends [never] ? 
+        [A, B] : [B, A]
 
 // insert an element into a sorted list in ascending order
-type InsertElement<
-    Item extends number, 
-    List extends number[]=[],
->=
+type InsertElement<Item extends number, List extends number[]=[]>=
     List['length'] extends 0 ? 
         [Item] : 
     List['length'] extends 1 ? 
-        Compare<List[0], Item> :
+        Compare<Item, List[0]> :
     List extends [infer First, ...infer Rest] ?
         [IsGreater<Item, First>] extends [never] ?
             [Item, First, ...Rest] :
             [First, ...InsertElement<Item, Rest>]
     : never
 
-type TestInsert0 = InsertElement<1, []>
-//   ^?
 
-type TestInsert1 = InsertElement<1, [0]>
-//   ^?
-
-type TestInsert2 = InsertElement<5, [3, 9]>
-//   ^?
-
-type TestInsert3 = InsertElement<8, [5, 10, 15]>
-//   ^?
-
-type TestInsert4 = InsertElement<8, [5, 8, 10, 15]>
-//   ^?
-
-type R = InsertElement<13, [1, 3, 4, 6, 9]>
-//   ^?
-
-
-type MergeSort<List extends number[]> =
+/**
+ * Sorted - Utility Type
+ * 
+ * Takes a tuple of unsorted numbers and returns a sorted tuple of the
+ * same values in ascending order. Can handle negative as well as 
+ * duplicate numbers.
+ * 
+ * This works by destructuring the list into [List[0], ...List.slice(1)]
+ * sorting the list slice recursively until the slice is only one element.
+ * Then as we return we insert List[0] at each level into a sorted list.
+ * 
+ * To change the order please see the IsGreater type above. Note that
+ * this will not work for floating point numbers (yet).
+ */
+type Sorted<List extends number[]> =
     List['length'] extends 0 ? [] :
     List['length'] extends 1 ? List :
     List extends [infer First, ...infer Rest] ?
-        InsertElement<First, MergeSort<Rest>>
+        InsertElement<First, Sorted<Rest>>
     : never
 
 
-type MergeSortTest1 = MergeSort<[5, 4, 7, 2, 3, 1, 8, 9, 6]>
+
+
+/*  *   *   *   *   test cases  *   *   *   *   */
+
+
+
+
+type MergeSortTest0 = Sorted<[]>
 //   ^?
 
-type MergeSortTest2 = MergeSort<[1]>
+type MergeSortTest1 = Sorted<[0]>
 //   ^?
 
-type MergeSortTest3 = MergeSort<[10, 8]>
+type MergeSortTest2 = Sorted<[10, 8]>
 //   ^?
 
-type MergeSortTest5 = MergeSort<[9, 8, 7, 6, 5, 4, 3, 2, 1, 1]>
+type MergeSortTest3 = Sorted<[2, 1, 3]>
+//   ^?
+
+type MergeSortTest4 = Sorted<[5, 2, 1, 3, 4]>
+//   ^? 
+
+type MergeSortTest4 = Sorted<[1, 6, 1, 3, 5, 6, 2, 3, 5]>
+//   ^?
+
+type MergeSortTest5 = Sorted<[9, 8, 7, 6, 5, 4, 3, 2, 1, 1]>
 //   ^?
